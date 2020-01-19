84d0729d986754f5252ba7b9a626ad855b46498e
Gabriel Medina <gabrielsantos@digix.com.br>
2020-01-19 15:57:14 -0400

    Fixed indentation

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -17,23 +17,23 @@ module.exports = {
     if (!dev) {
       const github_response = await axios.get(`https://api.github.com/users/${github_username}`);
     
-    const { name = login, avatar_url, bio } = github_response.data;
-  
-    const techs_array = parseStringAsArray(techs);
-  
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    };
-  
-    dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techs_array,
-      location
-    });
+      const { name = login, avatar_url, bio } = github_response.data;
+    
+      const techs_array = parseStringAsArray(techs);
+    
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
+    
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techs_array,
+        location
+      });
     }
   
     return response.json(dev);
